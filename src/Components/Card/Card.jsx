4074b23c230a2ec0db47e6a74de156afe45e7dba
CedricFriscia <cedricfriscia@gmail.com>
2024-01-04 10:53:30 +0100

    change public public path

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -43,31 +43,31 @@ const Card = () => {
         <h2 className="card__title">Compétences</h2>
         <div className="card__skills">
           <img
-            src="src/assets/icons/kisspng-responsive-web-design-html-world-wide-web-consorti-5ae60d381c26f6.3859365115250261041153.png"
+            src="icons/kisspng-responsive-web-design-html-world-wide-web-consorti-5ae60d381c26f6.3859365115250261041153.png"
             alt=""
           />
 
           <img
-            src="src/assets/icons/kisspng-css3-cascading-style-sheets-computer-icons-html-emblem-5ac245f107ae31.3014101515226813290315.png"
+            src="icons/kisspng-css3-cascading-style-sheets-computer-icons-html-emblem-5ac245f107ae31.3014101515226813290315.png"
             alt=""
           />
 
           <img
-            src="src/assets/icons/kisspng-game-react-native-javascript-android-physics-5ac6d5f51879e8.6623465115229803411003.png"
+            src="icons/kisspng-game-react-native-javascript-android-physics-5ac6d5f51879e8.6623465115229803411003.png"
             alt=""
           />
 
           <img
-            src="src/assets/icons/kisspng-git-computer-icons-software-repository-software-de-price-tag-5ad04273211b63.7402327815235979391356.png"
+            src="icons/kisspng-git-computer-icons-software-repository-software-de-price-tag-5ad04273211b63.7402327815235979391356.png"
             alt=""
           />
 
           <img
-            src="src/assets/icons/kisspng-node-js-javascript-web-application-express-js-comp-5ae0f84e5e7537.0464945815246930703869.png"
+            src="icons/kisspng-node-js-javascript-web-application-express-js-comp-5ae0f84e5e7537.0464945815246930703869.png"
             alt=""
           />
           <img
-            src="src/assets/icons/kisspng-javascript-logo-html-comment-blog-5ae63c23139110.5436874015250381150802.png"
+            src="icons/kisspng-javascript-logo-html-comment-blog-5ae63c23139110.5436874015250381150802.png"
             alt=""
           />
         </div>
